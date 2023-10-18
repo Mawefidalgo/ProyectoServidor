06a0ce9a076e4d03d8ac7c8bd776cc22b005bf31
Mawefidalgo <146815063+Mawefidalgo@users.noreply.github.com>
2023-10-18 10:48:29 +0200

    Delete ManipulacionDatos.py

--- ManipulacionDatos.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# Leemos el archivo JSON
-with open("teams.json") as archivo_json:
-    teams = json.load(archivo_json)
-
-
-#Damos la bienvenida a la aplicacion
-print("Bienvenido a la aplicacion de la liga mexicana")
